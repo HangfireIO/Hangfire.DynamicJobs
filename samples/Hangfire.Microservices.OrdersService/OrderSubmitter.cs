8fb70c95ee303c03e291e8ca64151a15de4b49c9
Nicola Avancini <nicola.avancini@saidea.it>
2023-06-14 12:05:22 +0200

    update sample for use display name

--- samples/Hangfire.Microservices.OrdersService/OrderSubmitter.cs
+++ samples/Hangfire.Microservices.OrdersService/OrderSubmitter.cs
@@ -2,8 +2,10 @@ using System;
 
 namespace Hangfire.Microservices.OrdersService
 {
+    [Queue("orders")]
     public class OrderSubmitter
     {
+        [JobDisplayName("order {0} - status {1}")]
         public void Execute(long orderId, string status)
         {
             Console.WriteLine($"Submitting order {orderId} with status {status}");
