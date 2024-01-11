ef0580a800d6deefe15ea34a6ef45a7125be21c6
Sergey Odinokov <odinserj@hangfire.io>
2024-01-11 16:13:16 +0700

    Enable source link support with embedded symbols

--- psake-project.ps1
+++ psake-project.ps1
@@ -4,15 +4,11 @@ Include "packages\Hangfire.Build.0.2.6\tools\psake-common.ps1"
 Task Default -Depends Collect
 Task CI -Depends Pack
 
-Task RestoreCore -Depends Clean {
-    Exec { dotnet restore }
-}
-
-Task CompileCore -Depends RestoreCore {
+Task Build -Depends Clean -Description "Restore all the packages and build the whole solution." {
     Exec { dotnet build -c Release }
 }
 
-Task Test -Depends CompileCore -Description "Run unit and integration tests." {
+Task Test -Depends Build -Description "Run unit and integration tests." {
     Exec { dotnet test -c Release "tests\Hangfire.DynamicJobs.Tests\Hangfire.DynamicJobs.Tests.csproj" }
 }
 
@@ -31,5 +27,25 @@ Task Pack -Depends Collect -Description "Create NuGet packages and archive files
     $version = Get-PackageVersion
 
     Create-Archive "Hangfire.DynamicJobs-$version"
-    Create-Package "Hangfire.DynamicJobs" $version
+    Create-Package2 "Hangfire.DynamicJobs" $version
+}
+
+function Create-Package2($project, $version) {
+    Write-Host "Creating NuGet package for '$project'..." -ForegroundColor "Green"
+
+    Create-Directory $temp_dir
+    Copy-Files "$nuspec_dir\$project.nuspec" $temp_dir
+
+    $commit = (git rev-parse HEAD)
+
+    Try {
+        Write-Host "Patching version with '$version'..." -ForegroundColor "DarkGray"
+        Replace-Content "$nuspec_dir\$project.nuspec" '%version%' $version
+        Write-Host "Patching commit hash with '$commit'..." -ForegroundColor "DarkGray"
+        Replace-Content "$nuspec_dir\$project.nuspec" '%commit%' $commit
+        Exec { .$nuget pack "$nuspec_dir\$project.nuspec" -OutputDirectory "$build_dir" -BasePath "$build_dir" -Version "$version" }
+    }
+    Finally {
+        Move-Files "$temp_dir\$project.nuspec" $nuspec_dir
+    }
 }
