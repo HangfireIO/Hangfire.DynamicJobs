5a293f14767ed9b8ad08c742aefdb04522b21cf9
Sergey Odinokov <odinserj@hangfire.io>
2025-02-13 15:40:52 +0700

    Bump version to 0.3.0 and add release notes

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 
 // Please don't edit manually, use the `build.bat version` command instead
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.3.0")]
